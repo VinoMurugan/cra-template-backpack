60884a4c0045e79423752b91457c37c84077af6e
VinoMurugan <137902991+VinoMurugan@users.noreply.github.com>
2024-08-02 12:19:11 -0400

    Update App.js

--- template/src/App.js
+++ template/src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { BpkCode } from '@skyscanner/backpack-web/bpk-component-code';
 import BpkButton from '@skyscanner/backpack-web/bpk-component-button';
 import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import BpkCalendar from 'bpk-component-calendar'; // Import the Calendar component
 
 import { cssModules } from '@skyscanner/backpack-web/bpk-react-utils';
-
 import STYLES from './App.scss';
 
 const getClassName = cssModules(STYLES);
@@ -13,14 +13,19 @@ const App = () => (
   <div className={getClassName('App')}>
     <header className={getClassName('App__header')}>
       <div className={getClassName('App__header-inner')}>
-        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>Welcome to React + Backpack</BpkText>
+        <BpkText tagName="h1" textStyle="xxl" className={getClassName('App__heading')}>
+          Flight Schedule
+        </BpkText>
       </div>
     </header>
     <main className={getClassName('App__main')}>
       <BpkText tagName="p" className={getClassName('App__text')}>
-        To get started, edit <BpkCode>src/App.jsx</BpkCode> and save to reload.
+        To get started, edit <BpkCode>src/App.js</BpkCode> and save to reload.
       </BpkText>
-      <BpkButton onClick={() => alert('It works!')}>Click me</BpkButton>
+      <BpkCalendar className={getClassName('App__calendar')} /> {/* Add Calendar component */}
+      <BpkButton onClick={() => alert('It works!')} className={getClassName('App__button')}>
+        Continue
+      </BpkButton>
     </main>
   </div>
 );
